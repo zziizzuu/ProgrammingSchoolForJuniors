88dc12e1249235f97b83c1d40579891fc659d9e1
zziizzuu <bogdane_zidane@yahoo.com>
2015-03-10 11:38:16 +0200

    Homework No. 1
    Prime Factors Generator

---
+++ PSJ - PrimeFactors/PSJ - PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PSJ___PrimeFactors
+{
+    class PrimeFactors
+    {
+        public static List<Int32> Generate(Int32 number)
+        {
+            List<Int32> primes = new List<int> { 1 };
+            var factor = 2;
+
+            while (number > 1 && factor <= number)
+            {
+                while (number%factor == 0)
+                {
+                    number /= factor;
+                    primes.Add(factor);
+                }
+
+                factor++;
+            }
+
+            return primes;
+        }
+    }
+}
---
+++ PSJ - PrimeFactors/PSJ - PrimeFactors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PSJ___PrimeFactors
+{
+    class Program
+    {
+        private const int NUMBER = 24;
+
+        static void Main()
+        {
+            List<Int32> primeFactors = PrimeFactors.Generate(NUMBER);
+
+            Console.Write(NUMBER + " = ");
+
+            foreach (var factor in primeFactors)
+            {
+                if (factor == primeFactors.Last())
+                {
+                    Console.Write(factor);
+                    continue;
+                }
+                Console.Write(factor + " * ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
